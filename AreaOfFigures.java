2bad814543a64b37d36a2799e90b786097e03035
ralitsadzhukeva <123503031+ralitsadzhukeva@users.noreply.github.com>
2023-01-24 21:35:46 +0200

    Add files via upload

---
+++ AreaOfFigures.java
@@ -0,0 +1,26 @@
+package ConditionalStatements;
+
+import java.util.Scanner;
+public class AreaOfFigures {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String shape = scanner.nextLine();
+        double area = 0;
+        if (shape.equals("square")) {
+            double a = Double.parseDouble(scanner.nextLine());
+            area = a *a;
+        } else if (shape.equals("rectangle")) {
+            double a = Double.parseDouble(scanner.nextLine());
+            double b = Double.parseDouble(scanner.nextLine());
+            area = a * b;
+        } else if (shape.equals("circle")) {
+            double r = Double.parseDouble(scanner.nextLine());
+            area = Math.PI * r *r;
+        } else if (shape.equals("triangle")) {
+            double a = Double.parseDouble(scanner.nextLine());
+            double h = Double.parseDouble(scanner.nextLine());
+            area = a*h/2;
+        }
+        System.out.printf("%.3f",area);
+    }
+}
---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+package ConditionalStatements;
+
+import java.util.Scanner;
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+        if (num %2 == 0){
+            System.out.println("even");
+        }else {
+            System.out.println("odd");
+        }
+    }
+}
